be001a387fa5590edd1ffc8ce9628b547e83d33e
Matthias Heinzel <mheinzel@posteo.de>
2018-12-03 09:50:04 +0000

    2018-03: part 1

--- 2018/03/Main.hs
+++ 2018/03/Main.hs
@@ -1,12 +1,57 @@
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
 import Test.HUnit
 
-one :: String -> Int
-one = const 0
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.List
+import qualified Text.Parsec as P
+import Text.Parsec.String (Parser)
+
+-- one :: String -> Int
+one
+  = length . Map.filter (> 1) . count . foldMap affectedTiles . parseClaims
+
+count :: (Foldable f, Ord a) => f a -> Map.Map a Int
+count
+  = foldl' (\m k -> Map.insertWith (+) k 1 m) Map.empty
+
+affectedTiles :: Claim -> [(Int, Int)]
+affectedTiles Claim{..}
+  = (,) <$> take sizeX [posX..] <*> take sizeY [posY..]
+
+data Claim
+  = Claim { claimId, posX, posY, sizeX, sizeY :: !Int }
+  deriving (Eq, Show)
+
+parseClaims :: String -> [Claim]
+parseClaims
+  = either (error . show) id . P.parse (P.endBy claimP P.newline) ""
+
+claimP :: Parser Claim
+claimP
+  = do
+      P.char '#'
+      claimId <- int
+      P.string " @ "
+      posX <- int
+      P.char ','
+      posY <- int
+      P.string ": "
+      sizeX <- int
+      P.char 'x'
+      sizeY <- int
+      pure Claim{..}
+  where
+    int = read <$> P.many1 P.digit
+
+
 
 two :: String -> Int
-two = const 0
+two
+  = const 0
 
 main = do
   runTests "one" one oneTests
