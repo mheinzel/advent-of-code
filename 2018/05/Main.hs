7934a1e442aa19e45abe8019211a829b6edc8823
Matthias Heinzel <mheinzel@posteo.de>
2018-12-06 00:46:53 +0000

    2018-05: part one

--- 2018/05/Main.hs
+++ 2018/05/Main.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
 import Test.HUnit
 
+import qualified Data.Char as Char
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 import Data.Maybe
@@ -11,7 +13,21 @@ import Safe
 
 one :: String -> Int
 one
-  = const 0
+  = length . reduce . filter Char.isAlpha
+
+reduce :: [Char] -> [Char]
+reduce = flip foldl' [] $ \case
+  [] -> pure
+  (x:xs) -> \c -> react c x ++ xs
+
+react :: Char -> Char -> [Char]
+react x y
+  | Char.toLower x == Char.toLower y
+  , x /= y
+  = []
+
+  | otherwise
+  = [x, y]
 
 two :: String -> Int
 two
