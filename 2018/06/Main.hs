1dfa16c2e2c747ed5a694c9fa15a57f75eb9de70
Matthias Heinzel <mheinzel@posteo.de>
2018-12-06 07:58:52 +0000

    2018-06: template

---
+++ 2018/06/Main.hs
@@ -0,0 +1,43 @@
+module Main where
+
+import Test.HUnit
+
+import qualified Data.Char as Char
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.List
+import Data.Foldable
+import Data.Function (on)
+import Safe
+
+one :: String -> Int
+one
+  = const 0
+
+two :: String -> Int
+two
+  = const 0
+
+main = do
+  runTests "one" one oneTests
+  runTests "two" two twoTests
+  input <- readFile "input.txt"
+  print $ one input
+  print $ two input
+
+oneTests =
+  []
+
+twoTests =
+  []
+
+runTests
+  :: (Show a, Eq b, Show b)
+  => String -> (a -> b) -> [(a, b)] -> IO Counts
+runTests name f ts =
+  runTestTT $ TestList $
+    zipWith TestLabel (map ((name ++) . show) [1..]) $
+      flip map ts $ \(input, expected) ->
+        TestCase $
+          assertEqual (show input) expected (f input)
